c9952de6b76c5f0552004a1e98693a54501cfe5a
skhadge <suchit.khadge@gmail.com>
2022-09-20 11:03:22 -0700

    Added test method for FIZZ

--- src/test/java/com/tlglearning/FizzBuzzTest.java
+++ src/test/java/com/tlglearning/FizzBuzzTest.java
@@ -2,11 +2,20 @@ package com.tlglearning;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 class FizzBuzzTest {
 
   @Test
   void valueOf() {
+//    assert Set.of(FizzBuzz.FIZZ).equals(FizzBuzz.valueOf(6)); // OR can be changed to below code
+    assertEquals(Set.of(FizzBuzz.FIZZ) , FizzBuzz.valueOf(6));
+
+
+
+
+
+
   }
 }
