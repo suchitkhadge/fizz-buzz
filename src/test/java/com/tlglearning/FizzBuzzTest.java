cca1c85e8daa7d6a32240d12f40e5af6249f0a7e
skhadge <suchit.khadge@gmail.com>
2022-09-20 12:38:04 -0700

    Added test method for all different possible outcomes.

--- src/test/java/com/tlglearning/FizzBuzzTest.java
+++ src/test/java/com/tlglearning/FizzBuzzTest.java
@@ -8,14 +8,24 @@ import org.junit.jupiter.api.Test;
 class FizzBuzzTest {
 
   @Test
-  void valueOf() {
+  void valueOf_fizz() {
 //    assert Set.of(FizzBuzz.FIZZ).equals(FizzBuzz.valueOf(6)); // OR can be changed to below code
-    assertEquals(Set.of(FizzBuzz.FIZZ) , FizzBuzz.valueOf(6));
+    assertEquals(Set.of(FizzBuzz.FIZZ), FizzBuzz.valueOf(6));
+  }
 
+  @Test
+  void valueOf_buzz() {
+      assertEquals(Set.of(FizzBuzz.BUZZ) , FizzBuzz.valueOf(10));
+    }
 
+  @Test
+  void valueOf_fizzBuzz(){    //Equality of SET is not order dependent, equality of LIST is order dependent.
+    assertEquals(Set.of(FizzBuzz.FIZZ, FizzBuzz.BUZZ), FizzBuzz.valueOf(15));
+  }
 
-
-
+  @Test
+  void valueOf_none(){    //Equality of SET is not order dependent, equality of LIST is order dependent.
+    assertEquals(Set.of(), FizzBuzz.valueOf(16));
+  }
 
   }
-}
